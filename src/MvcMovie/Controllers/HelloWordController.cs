ad8a168d4fb30ac3c560a5d701782077044fbfa3
Marília Paiva <mariliapaivac@hotmail.com>
2015-04-28 20:57:49 -0300

    fonte projeto

---
+++ src/MvcMovie/Controllers/HelloWordController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWordController : Controller
+    {  
+        // GET: HelloWord
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("helloword/welcome/{name}/{numtimes?}")]
+        ////[Route("helloword/welcome/{name}")]
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello "+ name + ", NumTime is :"+ numTimes);
+        //}
+    }
+
+
+}
